f7b554b5276f4082c3f831a185c749300105e1eb
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-13 21:23:08 +0000

    Update comment style

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
-// NAME OF PROJECT (FILE)
-// DESCRIPTION OF PROJECT
-// GITHUB LINK
-//
-// INFOMATION ABOUT FILE
+/* NAME OF PROJECT (FILE)
+ * DESCRIPTION OF PROJECT
+ * GITHUB LINK
+ *
+ * INFOMATION ABOUT FILE
+*/
 
 #include <iostream>
 
 #include "include/main.h"
 
+/**
+ * The main function of the program.
+ * @argc: The number of arguments passed to the program.
+ * @argv: The arguments passed to the program.
+ * @return: The exit code of the program.
+*/
 int main(int argc, char *argv[]) {
     std::cout << hello_world << std::endl;
     return 0;
